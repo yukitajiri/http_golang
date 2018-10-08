e8ba0b25844b68d96a62942329365740f0c64b61
yuki-tajiri <yuki.tajiri@dena.com>
2018-10-08 11:47:49 +0900

    Getメソッドからレスポンスボディーを返す

--- main.go
+++ main.go
@@ -1,28 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"net/http/httputil"
 )
 
 
-
-func handler(w http.ResponseWriter, r *http.Request){
-	dump, err := httputil.DumpRequest(r ,true)
-	if err != nil {
-		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
-		return
-	}
-	fmt.Println(string(dump))
-	fmt.Fprintf(w, "<html><body>hello</body></html>\n")
-}
-
+// Getメソッドからレスポンスボディーを返す
 func main() {
-	var httpServer http.Server
-	http.HandleFunc("/", handler)
-	log.Println("start http listening :18888")
-	httpServer.Addr = ":18888"
-	log.Println(httpServer.ListenAndServe())
+	response, err := http.Get("http://localhost:18888")
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(body))
 }
